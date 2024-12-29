cff5ee5c4e6ad2bd264096b32dc07a53df81f64d
tanmay4269 <tanmay4269@gmail.com>
2024-12-29 04:06:14 +0000

    many lines changed, most of them are coz of reformating

--- isegm/model/ifss_hrnet_model.py
+++ isegm/model/ifss_hrnet_model.py
@@ -8,24 +8,25 @@ from .modeling.hrnet_ocr import HighResolutionNet
 from isegm.model.modifiers import LRMult
 
 
+# TODO: Add some customization explanations
 class CustomHRNet(HighResolutionNet):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        
+
         self.conv_0 = nn.Conv2d(256 + 18, 256, 1)
         self.conv_1 = nn.Conv2d(36 + 36, 36, 1)
         self.conv_2 = nn.Conv2d(72 + 72, 72, 1)
         self.conv_3 = nn.Conv2d(144 + 144, 144, 1)
-    
+
     def forward(self, x, additional_features=None):
         x, feats_list = self.encoder(x, additional_features)
         output = self.decoder(x)
-        
+
         return output, feats_list
-    
+
     def encoder(self, x, additional_features):
         return self.compute_hrnet_feats(x, additional_features)
-        
+
     def decoder(self, feats):
         if self.ocr_width > 0:
             out_aux = self.aux_head(feats)
@@ -37,26 +38,31 @@ class CustomHRNet(HighResolutionNet):
             return [out, out_aux]
         else:
             return [self.cls_head(feats), None]
-        
+
     def aggregate_hrnet_features(self, x):
         # Upsampling
         x0_h, x0_w = x[0].size(2), x[0].size(3)
-        x1 = F.interpolate(x[1], size=(x0_h, x0_w),
-                           mode='bilinear', align_corners=self.align_corners)
-        x2 = F.interpolate(x[2], size=(x0_h, x0_w),
-                           mode='bilinear', align_corners=self.align_corners)
-        x3 = F.interpolate(x[3], size=(x0_h, x0_w),
-                           mode='bilinear', align_corners=self.align_corners)
+        x1 = F.interpolate(
+            x[1], size=(x0_h, x0_w), mode="bilinear", align_corners=self.align_corners
+        )
+        x2 = F.interpolate(
+            x[2], size=(x0_h, x0_w), mode="bilinear", align_corners=self.align_corners
+        )
+        x3 = F.interpolate(
+            x[3], size=(x0_h, x0_w), mode="bilinear", align_corners=self.align_corners
+        )
 
         return torch.cat([x[0], x1, x2, x3], 1), x
-    
+
     def query_encoder(self, x, prototypes):
         """
         Iteration 1: conv1x1 is used
         """
-        x = self.compute_pre_stage_features(x, additional_features=None)  # (64, 80, 120)
+        x = self.compute_pre_stage_features(
+            x, additional_features=None
+        )  # (64, 80, 120)
         x = self.layer1(x)  # (256, 80, 120)
-        
+
         _, _, H, W = x.shape
         prototype = prototypes[0].unsqueeze(2).unsqueeze(3).repeat(1, 1, H, W)
         x = torch.cat((x, prototype), dim=1)
@@ -71,12 +77,12 @@ class CustomHRNet(HighResolutionNet):
         y_list = self.stage2(x_list)
         # (18, 80, 120)
         # (36, 40, 60)
-        
+
         _, _, H, W = y_list[-1].shape
         prototype = prototypes[1].unsqueeze(2).unsqueeze(3).repeat(1, 1, H, W)
         x = torch.cat((y_list[-1], prototype), dim=1)
         y_list[-1] = self.conv_1(x)
-        
+
         x_list = []
         for i in range(self.stage3_num_branches):
             if self.transition2[i] is not None:
@@ -90,13 +96,12 @@ class CustomHRNet(HighResolutionNet):
         # (18, 80, 120)
         # (36, 40, 60)
         # (72, 20, 30)
-        
+
         _, _, H, W = y_list[-1].shape
         prototype = prototypes[2].unsqueeze(2).unsqueeze(3).repeat(1, 1, H, W)
         x = torch.cat((y_list[-1], prototype), dim=1)
         y_list[-1] = self.conv_2(x)
-        
-        
+
         x_list = []
         for i in range(self.stage4_num_branches):
             if self.transition3[i] is not None:
@@ -111,91 +116,95 @@ class CustomHRNet(HighResolutionNet):
         # (36, 40, 60)
         # (72, 20, 30)
         # (144, 10, 15)
-        
+
         _, _, H, W = y_list[-1].shape
         prototype = prototypes[3].unsqueeze(2).unsqueeze(3).repeat(1, 1, H, W)
         x = torch.cat((y_list[-1], prototype), dim=1)
         y_list[-1] = self.conv_3(x)
-        
-        out, _ = self.aggregate_hrnet_features(y_list)
-        
-        return out
-        
 
-class HRNetModel(iFSSModel):
+        out, _ = self.aggregate_hrnet_features(y_list)
+
+        return out
+
+
+class iFSS_HRNetModel(iFSSModel):
     @serialize
-    def __init__(self, 
-            width=48, 
-            ocr_width=256, 
-            small=False, 
-            backbone_lr_mult=0.1,
-            norm_layer=nn.BatchNorm2d, 
-            **kwargs
+    def __init__(
+        self,
+        width=48,
+        ocr_width=256,
+        small=False,
+        backbone_lr_mult=0.1,
+        norm_layer=nn.BatchNorm2d,
+        **kwargs
     ):
         super().__init__(norm_layer=norm_layer, **kwargs)
-        
+
         # Support Branch
         self.support_net = CustomHRNet(
-            width=width, 
-            ocr_width=ocr_width, 
+            width=width,
+            ocr_width=ocr_width,
             small=small,
-            num_classes=1, 
-            norm_layer=norm_layer
+            num_classes=1,
+            norm_layer=norm_layer,
         )
-        
+
         self.support_net.apply(LRMult(backbone_lr_mult))
         if ocr_width > 0:
             self.support_net.ocr_distri_head.apply(LRMult(1.0))
             self.support_net.ocr_gather_head.apply(LRMult(1.0))
             self.support_net.conv3x3_ocr.apply(LRMult(1.0))
-            
+
         # Query Branch
         # Input
         use_leaky_relu = True
         query_input_layers = [
-            nn.Conv2d(in_channels=3+1, out_channels=6+1, kernel_size=1),
-            nn.BatchNorm2d(6+1),
-            nn.LeakyReLU(negative_slope=0.2) if use_leaky_relu else nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=6+1, out_channels=3, kernel_size=1)
+            nn.Conv2d(in_channels=3 + 1, out_channels=6 + 1, kernel_size=1),
+            nn.BatchNorm2d(6 + 1),
+            (
+                nn.LeakyReLU(negative_slope=0.2)
+                if use_leaky_relu
+                else nn.ReLU(inplace=True)
+            ),
+            nn.Conv2d(in_channels=6 + 1, out_channels=3, kernel_size=1),
         ]
-        
+
         self.query_input = nn.Sequential(*query_input_layers)
-        
+
         self.query_net = CustomHRNet(
-            width=width, 
-            ocr_width=ocr_width, 
+            width=width,
+            ocr_width=ocr_width,
             small=small,
-            num_classes=1, 
-            norm_layer=norm_layer
+            num_classes=1,
+            norm_layer=norm_layer,
         )
-        
+
         self.query_net.apply(LRMult(backbone_lr_mult))
         if ocr_width > 0:
             self.query_net.ocr_distri_head.apply(LRMult(1.0))
             self.query_net.ocr_gather_head.apply(LRMult(1.0))
             self.query_net.conv3x3_ocr.apply(LRMult(1.0))
-            
+
         # Debug: Support GT Branch
         self.support_gt_net = CustomHRNet(
-            width=width, 
-            ocr_width=ocr_width, 
+            width=width,
+            ocr_width=ocr_width,
             small=small,
-            num_classes=1, 
-            norm_layer=norm_layer
+            num_classes=1,
+            norm_layer=norm_layer,
         )
-        
+
         self.support_gt_net.apply(LRMult(backbone_lr_mult))
         if ocr_width > 0:
             self.support_gt_net.ocr_distri_head.apply(LRMult(1.0))
             self.support_gt_net.ocr_gather_head.apply(LRMult(1.0))
             self.support_gt_net.conv3x3_ocr.apply(LRMult(1.0))
-        
-        
+
     def support_forward(self, image, coord_features=None):
         outputs, feature_list = self.support_net(image, coord_features)
-        
+
         s_pred = (torch.sigmoid(outputs[0]) > 0.5).int()
-        
+
         prototypes = []
 
         for features in feature_list:
@@ -208,11 +217,11 @@ class HRNetModel(iFSSModel):
 
             prototype = torch.mean(features * pred, dim=(2, 3))
             prototypes.append(prototype)
-        
+
         return {
-            'instances': outputs[0], 
-            'instances_aux': outputs[1],
-            'prototypes': prototypes
+            "instances": outputs[0],
+            "instances_aux": outputs[1],
+            "prototypes": prototypes,
         }
 
     def query_forward(self, image, prev_output, prototypes, fss_pretrain_assist=None):
@@ -220,7 +229,7 @@ class HRNetModel(iFSSModel):
         if fss_pretrain_assist is not None:
             s_image, s_gt = fss_pretrain_assist
             _, feature_list = self.support_gt_net(s_image)
-        
+
             prototypes = []
 
             for features in feature_list:
@@ -233,11 +242,11 @@ class HRNetModel(iFSSModel):
 
                 prototype = torch.mean(features * pred, dim=(2, 3))
                 prototypes.append(prototype)
-        
+
         prev_output = torch.sigmoid(prev_output)
         x = self.query_input(torch.cat((image, prev_output), dim=1))
         x = self.query_net.query_encoder(x, prototypes)
-        
-        outputs = self.query_net.decoder(x)        
 
-        return {'masks': outputs[0], 'masks_aux': outputs[1]}
+        outputs = self.query_net.decoder(x)
+
+        return {"masks": outputs[0], "masks_aux": outputs[1]}
--- isegm/model/ifss_model.py
+++ isegm/model/ifss_model.py
@@ -7,11 +7,21 @@ from isegm.model.modifiers import LRMult
 
 
 class iFSSModel(nn.Module):
-    def __init__(self, use_rgb_conv=True, with_aux_output=False,
-                 norm_radius=260, use_disks=False, cpu_dist_maps=False,
-                 clicks_groups=None, with_prev_mask=False, use_leaky_relu=False,
-                 binary_prev_mask=False, conv_extend=False, norm_layer=nn.BatchNorm2d,
-                 norm_mean_std=([.485, .456, .406], [.229, .224, .225])):
+    def __init__(
+        self,
+        use_rgb_conv=True,
+        with_aux_output=False,
+        norm_radius=260,
+        use_disks=False,
+        cpu_dist_maps=False,
+        clicks_groups=None,
+        with_prev_mask=False,
+        use_leaky_relu=False,
+        binary_prev_mask=False,
+        conv_extend=False,
+        norm_layer=nn.BatchNorm2d,
+        norm_mean_std=([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+    ):
         super().__init__()
         self.with_aux_output = with_aux_output
         self.clicks_groups = clicks_groups
@@ -28,42 +38,77 @@ class iFSSModel(nn.Module):
 
         if use_rgb_conv:
             rgb_conv_layers = [
-                nn.Conv2d(in_channels=3 + self.coord_feature_ch, out_channels=6 + self.coord_feature_ch, kernel_size=1),
+                nn.Conv2d(
+                    in_channels=3 + self.coord_feature_ch,
+                    out_channels=6 + self.coord_feature_ch,
+                    kernel_size=1,
+                ),
                 norm_layer(6 + self.coord_feature_ch),
-                nn.LeakyReLU(negative_slope=0.2) if use_leaky_relu else nn.ReLU(inplace=True),
-                nn.Conv2d(in_channels=6 + self.coord_feature_ch, out_channels=3, kernel_size=1)
+                (
+                    nn.LeakyReLU(negative_slope=0.2)
+                    if use_leaky_relu
+                    else nn.ReLU(inplace=True)
+                ),
+                nn.Conv2d(
+                    in_channels=6 + self.coord_feature_ch, out_channels=3, kernel_size=1
+                ),
             ]
             self.rgb_conv = nn.Sequential(*rgb_conv_layers)
         elif conv_extend:
             self.rgb_conv = None
-            self.maps_transform = nn.Conv2d(in_channels=self.coord_feature_ch, out_channels=64,
-                                            kernel_size=3, stride=2, padding=1)
+            self.maps_transform = nn.Conv2d(
+                in_channels=self.coord_feature_ch,
+                out_channels=64,
+                kernel_size=3,
+                stride=2,
+                padding=1,
+            )
             self.maps_transform.apply(LRMult(0.1))
         else:
             self.rgb_conv = None
             mt_layers = [
-                nn.Conv2d(in_channels=self.coord_feature_ch, out_channels=16, kernel_size=1),
-                nn.LeakyReLU(negative_slope=0.2) if use_leaky_relu else nn.ReLU(inplace=True),
-                nn.Conv2d(in_channels=16, out_channels=64, kernel_size=3, stride=2, padding=1),
-                ScaleLayer(init_value=0.05, lr_mult=1)
+                nn.Conv2d(
+                    in_channels=self.coord_feature_ch, out_channels=16, kernel_size=1
+                ),
+                (
+                    nn.LeakyReLU(negative_slope=0.2)
+                    if use_leaky_relu
+                    else nn.ReLU(inplace=True)
+                ),
+                nn.Conv2d(
+                    in_channels=16, out_channels=64, kernel_size=3, stride=2, padding=1
+                ),
+                ScaleLayer(init_value=0.05, lr_mult=1),
             ]
             self.maps_transform = nn.Sequential(*mt_layers)
 
         if self.clicks_groups is not None:
             self.dist_maps = nn.ModuleList()
             for click_radius in self.clicks_groups:
-                self.dist_maps.append(DistMaps(norm_radius=click_radius, spatial_scale=1.0,
-                                               cpu_mode=cpu_dist_maps, use_disks=use_disks))
+                self.dist_maps.append(
+                    DistMaps(
+                        norm_radius=click_radius,
+                        spatial_scale=1.0,
+                        cpu_mode=cpu_dist_maps,
+                        use_disks=use_disks,
+                    )
+                )
         else:
-            self.dist_maps = DistMaps(norm_radius=norm_radius, spatial_scale=1.0,
-                                      cpu_mode=cpu_dist_maps, use_disks=use_disks)
+            self.dist_maps = DistMaps(
+                norm_radius=norm_radius,
+                spatial_scale=1.0,
+                cpu_mode=cpu_dist_maps,
+                use_disks=use_disks,
+            )
 
-    def forward(self, s_image, prev_s_output, s_points, q_image, prev_q_output, s_gt=None):
+    def forward(
+        self, s_image, prev_s_output, s_points, q_image, prev_q_output, s_gt=None
+    ):
         """
         Args: prev outputs are logits
         TODO: dont have a million things passed like this, concat and pass into this
         """
-        
+
         s_image, prev_s_mask = self.prepare_input(s_image, prev_s_output)
         coord_features = self.get_coord_features(s_image, prev_s_mask, s_points)
 
@@ -75,49 +120,50 @@ class iFSSModel(nn.Module):
             s_outputs = self.support_forward(s_image, coord_features)
 
         q_outputs = self.query_forward(
-            q_image, 
-            prev_q_output, 
-            s_outputs['prototypes'], 
-            fss_pretrain_assist=[s_image, s_gt] if s_gt is not None else None
+            q_image,
+            prev_q_output,
+            s_outputs["prototypes"],
+            fss_pretrain_assist=[s_image, s_gt] if s_gt is not None else None,
         )
-        
-        s_outputs['instances'] = nn.functional.interpolate(
-            s_outputs['instances'], 
-            size=s_image.size()[2:], 
-            mode='bilinear', 
-            align_corners=True
+
+        s_outputs["instances"] = nn.functional.interpolate(
+            s_outputs["instances"],
+            size=s_image.size()[2:],
+            mode="bilinear",
+            align_corners=True,
         )
-        
-        q_outputs['masks'] = nn.functional.interpolate(
-            q_outputs['masks'], 
-            size=q_image.size()[2:], 
-            mode='bilinear', 
-            align_corners=True
+
+        q_outputs["masks"] = nn.functional.interpolate(
+            q_outputs["masks"],
+            size=q_image.size()[2:],
+            mode="bilinear",
+            align_corners=True,
         )
-        
+
         if self.with_aux_output:
-            s_outputs['instances_aux'] = nn.functional.interpolate(
-                s_outputs['instances_aux'], 
+            s_outputs["instances_aux"] = nn.functional.interpolate(
+                s_outputs["instances_aux"],
                 size=s_image.size()[2:],
-                mode='bilinear', 
-                align_corners=True
+                mode="bilinear",
+                align_corners=True,
             )
-            
-            q_outputs['masks_aux'] = nn.functional.interpolate(
-                q_outputs['masks_aux'], 
+
+            q_outputs["masks_aux"] = nn.functional.interpolate(
+                q_outputs["masks_aux"],
                 size=q_image.size()[2:],
-                mode='bilinear', 
-                align_corners=True
+                mode="bilinear",
+                align_corners=True,
             )
-            
+
         outputs = {}
         for k, v in s_outputs.items():
-            if k == 'prototype': continue
-            outputs[f's_{k}'] = v
+            if k == "prototype":
+                continue
+            outputs[f"s_{k}"] = v
 
         for k, v in q_outputs.items():
-            outputs[f'q_{k}'] = v
-            
+            outputs[f"q_{k}"] = v
+
         return outputs
 
     def prepare_input(self, image, prev_mask):
@@ -127,7 +173,7 @@ class iFSSModel(nn.Module):
         #     image = image[:, :3, :, :]
         #     if self.binary_prev_mask:
         #         prev_mask = (prev_mask > 0.5).float()
-        
+
         if self.binary_prev_mask:
             prev_mask = (prev_mask > 0.5).float()
 
@@ -139,8 +185,13 @@ class iFSSModel(nn.Module):
 
     def get_coord_features(self, image, prev_mask, points):
         if self.clicks_groups is not None:
-            points_groups = split_points_by_order(points, groups=(2,) + (1, ) * (len(self.clicks_groups) - 2) + (-1,))
-            coord_features = [dist_map(image, pg) for dist_map, pg in zip(self.dist_maps, points_groups)]
+            points_groups = split_points_by_order(
+                points, groups=(2,) + (1,) * (len(self.clicks_groups) - 2) + (-1,)
+            )
+            coord_features = [
+                dist_map(image, pg)
+                for dist_map, pg in zip(self.dist_maps, points_groups)
+            ]
             coord_features = torch.cat(coord_features, dim=1)
         else:
             coord_features = self.dist_maps(image, points)
@@ -158,8 +209,7 @@ def split_points_by_order(tpoints: torch.Tensor, groups):
     num_points = points.shape[1] // 2
 
     groups = [x if x > 0 else num_points for x in groups]
-    group_points = [np.full((bs, 2 * x, 3), -1, dtype=np.float32)
-                    for x in groups]
+    group_points = [np.full((bs, 2 * x, 3), -1, dtype=np.float32) for x in groups]
 
     last_point_indx_group = np.zeros((bs, num_groups, 2), dtype=np.int)
     for group_indx, group_size in enumerate(groups):
@@ -173,7 +223,9 @@ def split_points_by_order(tpoints: torch.Tensor, groups):
                 continue
 
             is_negative = int(pindx >= num_points)
-            if group_id >= num_groups or (group_id == 0 and is_negative):  # disable negative first click
+            if group_id >= num_groups or (
+                group_id == 0 and is_negative
+            ):  # disable negative first click
                 group_id = num_groups - 1
 
             new_point_indx = last_point_indx_group[bindx, group_id, is_negative]
@@ -181,7 +233,9 @@ def split_points_by_order(tpoints: torch.Tensor, groups):
 
             group_points[group_id][bindx, new_point_indx, :] = point
 
-    group_points = [torch.tensor(x, dtype=tpoints.dtype, device=tpoints.device)
-                    for x in group_points]
+    group_points = [
+        torch.tensor(x, dtype=tpoints.dtype, device=tpoints.device)
+        for x in group_points
+    ]
 
     return group_points
